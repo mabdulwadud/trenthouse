ed6cbdd7da8a50ab0c458601cf90c5ca493a9550
Masum <masumabdulwadud@gmail.com>
2023-05-15 02:36:33 -0400

    updated mesh on 3D model and added more sections

--- index.js
+++ index.js
@@ -1,85 +1,26 @@
-// import * as THREE from 'three';
-// import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-// import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-
-
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-// document.getElementById("container3D").appendChild(renderer.domElement);
-
-// let object;
-// let controls;
-
-// const loader = new OBJLoader();
-
-// // load a resource
-// loader.load(
-// 	// resource URL
-// 	'models/trenthouseobj.obj',
-// 	// called when resource is loaded
-// 	function ( object ) {
-
-// 		scene.add( object );
-
-// 	},
-// 	// called when loading is in progresses
-// 	function ( xhr ) {
-
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-
-// 		console.log( 'An error happened' );
-
-// 	}
-// );
-
-
-
-// const renderer = new THREE.WebGLRenderer({alpha: true});
-// renderer.setSize( window.innerWidth, window.innerHeight );
-
-// // adding directional and ambient lighting to the scene
-// const topLight = new THREE.DirectionalLight(0xffffff, 1);
-// topLight.position.set(500,500,500);
-// topLight.castShadow = true;
-// scene.add(topLight);
-
-// const ambientLight = new THREE.AmbientLight(0x333333, 1);
-
-// function animate(){
-//     requestAnimationFrame(animate);
-
-//     window.addEventListener("resize", function(){
-//         camera.aspect = window.innerWidth / window.innerHeight;
-//         camera.updateProjectionMatrix();
-//         renderer.setSize(window.innerWidth, window.innerHeight);
-//     });
-
-//     renderer.render(scene, camera);
-// }
-
-// animate();
-
-import * as THREE from 'three';
+import * as THREE from "three";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 
-
-// init
-
+// initialize threejs scene
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.90, 1000 );
-const renderer = new THREE.WebGLRenderer({alpha:true});
-document.getElementById('container3D').appendChild( renderer.domElement );
+// create camera and renderer with transparent bg
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+document.getElementById("container3D").appendChild(renderer.domElement);
 
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-camera.position.z = 200;
+camera.position.set(-100, -30, 30);
+
+// enable camera controls
 
 var controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -87,40 +28,76 @@ controls.campingFactor = 0.25;
 controls.enableZoom = true;
 controls.autoRotate = true;
 
-  
-// var ambientLight = new THREE.AmbientLight(0x413e4a, 0.4);
-// ambientLight.position.set(100,0,100);
-// var fillLight = new THREE.DirectionalLight(0x6EEAE2, 0.1);
-// fillLight.position.set(-50,0,10);
-// fillLight.castShadow = true;
-
+//point light 1
 var pointLight1 = new THREE.PointLight(0xffffff, 0.3);
 pointLight1.castShadow = true;
-pointLight1.position.set(0,0,100);
+pointLight1.position.set(0, 0, 100);
 
+//point light 2
 var pointLight2 = new THREE.PointLight(0xffffff, 0.3);
 pointLight2.castShadow = true;
-pointLight2.position.set(0,0,-100);
+pointLight2.position.set(0, 0, -100);
 
-
+// adding both lights to the scene
 scene.add(pointLight1);
 scene.add(pointLight2);
 
-
-var objLoader = new OBJLoader();
-objLoader.setPath('/models/');
-objLoader.load('trenthouseobj.obj', function(object){
-    object.position.y -= 60;
-    scene.add(object);
+// loading material
+var myObj = undefined;
+var mtlLoader = new MTLLoader();
+mtlLoader.load("models/model.mtl", function(materials){
+    materials.preload();
+    //loading object
+    var objLoader = new OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.load("models/model.obj", function(object){
+        // setting undefined mesh to object
+        myObj = object;
+        scene.add(myObj);
+    });
 });
 
 // animation
-
-
 function animate() {
-    requestAnimationFrame(animate);
-    controls.update();
-	renderer.render( scene, camera );
-
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
 }
 animate();
+
+// Get the container3D element
+const container3D = document.getElementById('container3D');
+
+// Create a new overlay element
+const overlay = document.createElement('div');
+overlay.className = 'overlay';
+overlay.textContent = 'Swipe or pinch to interact';
+
+// Apply styling to the overlay
+overlay.style.position = 'absolute';
+overlay.style.top = '50%';
+overlay.style.left = '50%';
+overlay.style.transform = 'translate(-50%, -50%)';
+overlay.style.fontSize = '24px';
+overlay.style.color = 'white';
+overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+overlay.style.padding = '10px';
+overlay.style.borderRadius = '5px';
+overlay.style.transition = 'opacity 1s';
+
+// Add the overlay to the container3D element
+container3D.appendChild(overlay);
+
+// Fade out the overlay after 3 seconds
+setTimeout(function () {
+  overlay.style.opacity = '0';
+}, 3000);
+
+
+const scrollActive = document.getElementById("description");
+const arrow = document.getElementById("arrow-down");
+
+arrow.addEventListener("click", function(){
+    scrollActive.scrollIntoView({behavior:"smooth", block:"end", inline:"nearest"});
+});
+
